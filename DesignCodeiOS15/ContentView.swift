8e80a197d044a17a24ece0b37063989b734c5dbf
hope <hope@gravylab.co.kr>
2022-01-03 22:39:27 +0900

    Visual Editor Practice

--- DesignCodeiOS15/ContentView.swift
+++ DesignCodeiOS15/ContentView.swift
@@ -10,8 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("SwiftUI for iOS15")
-            .padding()
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(20.0)
+            Text("SwiftUI for iOS15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("20 sections - 3 hours")
+                .font(.footnote)
+                .fontWeight(.semibold)
+            Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+        }
+            .padding(.all, 20.0)
+            .frame(height: 350.0)
+            .background(Color("Background"))
+            .cornerRadius(30.0)
+            .shadow(radius: 20)
+
     }
 }
 
