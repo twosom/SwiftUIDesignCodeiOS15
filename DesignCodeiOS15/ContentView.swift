e72d0d1cc2225a6127595d37eb230b7d7041d744
hope <hope@gravylab.co.kr>
2022-01-04 01:30:23 +0900

    Background Material Setting

--- DesignCodeiOS15/ContentView.swift
+++ DesignCodeiOS15/ContentView.swift
@@ -10,35 +10,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+
         VStack(alignment: .leading, spacing: 8.0) {
             Spacer()
             Image("Logo 2")
-                .resizable(resizingMode: .stretch)
+                .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: 26.0, height: 26.0)
-                .cornerRadius(20.0)
-                .padding()
-            Text("SwiftUI for iOS15")
-                .font(.largeTitle)
-                .fontWeight(.bold)
+                .frame(width: 26, height: 26)
+                .clipShape(Circle())
+
+            Text("SwiftUI for iOS 15")
+                .font(.largeTitle.bold())
+                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .lineLimit(1)
+
             Text("20 sections - 3 hours".uppercased())
-                .font(.footnote)
-                .fontWeight(.semibold)
-                .foregroundColor(.secondary)
-            Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                .font(.footnote.weight(.semibold))
+                .foregroundStyle(.secondary)
+            Text("Build an iOS app for iOS 15 with custom layouts, animation and ...")
                 .font(.footnote)
                 .multilineTextAlignment(.leading)
                 .lineLimit(2)
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .foregroundColor(.secondary)
+
+
         }
             .padding(.all, 20)
             .padding(.vertical, 20)
-            .frame(height: 350.0)
-            .background(Color("Background"))
-            .cornerRadius(30.0)
+            .frame(height: 350)
+            .background(.ultraThinMaterial)
+            .cornerRadius(30)
             .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
             .padding(.horizontal, 20)
+            .background(
+                    Image("Blob 1")
+                        .offset(x: 250, y: -100)
+            )
+            .overlay(
+                    Image("Illustration 5")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 230)
+                        .offset(x: 32, y: -100)
+
+            )
 
 
     }
@@ -50,7 +66,6 @@ struct ContentView_Previews: PreviewProvider {
             ContentView()
             ContentView()
                 .preferredColorScheme(.dark)
-                .environment(\.sizeCategory, .accessibilityLarge)
                 .previewDevice("iPhone 13 mini")
         }
     }
