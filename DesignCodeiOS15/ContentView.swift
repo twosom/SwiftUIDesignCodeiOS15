8e3f0ef9cccc3a2f7fea70e4873d6ed6018666bc
hope <hope@gravylab.co.kr>
2022-01-04 02:03:15 +0900

    Stroke and Blend Mode

--- DesignCodeiOS15/ContentView.swift
+++ DesignCodeiOS15/ContentView.swift
@@ -17,7 +17,9 @@ struct ContentView: View {
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26, height: 26)
-                .clipShape(Circle())
+                .mask(Circle())
+                .padding(9)
+                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 20, style: .continuous))
 
             Text("SwiftUI for iOS 15")
                 .font(.largeTitle.bold())
@@ -34,14 +36,17 @@ struct ContentView: View {
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .foregroundColor(.secondary)
 
-
         }
             .padding(.all, 20)
             .padding(.vertical, 20)
             .frame(height: 350)
-            .background(.ultraThinMaterial)
-            .cornerRadius(30)
+            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+            .overlay(
+                    RoundedRectangle(cornerRadius: 30, style: .continuous)
+                        .stroke(.linearGradient(colors: [.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
+                        .blendMode(.overlay)
+            )
             .padding(.horizontal, 20)
             .background(
                     Image("Blob 1")
